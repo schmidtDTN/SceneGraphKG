9c29afc6cf96e5d61dd8ed598a940634229dc721
Daniel Schmidt <schmidt.152@wright.edu>
2020-09-17 10:02:57 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+This is a prototype of a scene graph question-answering system which can detect (and eventually resolve) knowledge gaps.
+
+Right now, it takes in a GraphML scene graph (currently not passed in but rather set in a global variable in the file) and handles user queries regarding existence of certain objects or relations between certain objects.
+
+Also, it checks for context gaps on graph load and returns a list of the nodes which have no edges connected to them.  The list is currently unused but may be useful at some point in the future.
+
+The format for the possible queries which can be entered are as follow:
+    exists(objectName) - queries the existence of an object, equivalent to asking "is there a objectName?"  Format for asking about the existence of a bat would be: exists(bat)
+
+    relation(relationString,object1Name,object2Name) - queries whether the two objects in question are connected by an edge with a label that EXACTLY matches the relation string.  i.e. if you want to ask if player and hat have a "to the left of" edge connecting them, the format would be:
+    relation(to the left of,player,hat)
+
---
+++ SceneGraphProcessing.py
@@ -0,0 +1,110 @@
+import networkx
+import re
+
+scene_graph_file = "2377804_scene_graph.graphml"
+existence_keyword = "exists"
+relation_keyword = "relation"
+attribute_keyword = "attribute"
+
+''' Note for Goonmeet: The way Lexical Gaps are identified here might not be ideal - should "this item isn't in the node"
+ be treated as a gap or just a "nope, not present"?  We may need to put our heads together on that one; I think it
+ could be argued that it's fine as is, because technically asking a question about the existence of something which
+ doesn't exist opens up a lexical gap, even if it's an acceptable one?  Same goes for the target gap, at least 
+ in the existence query'''
+
+
+# Nodes are items (eventually attributes also), edges are positioning relationships (or eventually hasAttribute)
+
+# ASSUMPTIONS:
+# Users enter queries correct conforming to the format.
+# When the user asks about, say "jersey", it's reasonable to look at jersey, jersey_1, and jersey_2; thus, the "_X"
+# will be ignored
+
+
+# Potential Graph modifications to simplify things: Add a tag of some sort that identifies objects as objects and
+# attributes as such.  i.e. name: pants, type: object; name: orange, type: attribute
+
+# Check each node in the scene graph to see if it has edges connected to it.  If it has no edges connected to it, flag
+# a context gap on that node.
+def contextGapCheck(sceneGraph):
+    # Making a list of all the nodes that have context gaps.  It's unused at this point, but it may be useful at some
+    # point?
+    contextGappedNodes = []
+
+    # Maybe not the most efficient thing - proof of concept, optimization can come later
+    # Iterate through all the nodes of the graph, check their in/out edges and see if there's any that just don't have
+    # edges
+    for currentNode in sceneGraph.nodes:
+        # Grab all the edges coming in and out of the current node and see if there are none.
+        nodeOutEdges = sceneGraph.out_edges(currentNode)
+        nodeInEdges = sceneGraph.in_edges(currentNode)
+        allNodeEdges = list(nodeOutEdges) + list(nodeInEdges)
+        if allNodeEdges == []:
+            # If no edges connected to the node, a context gap may be in order.
+            print("WARNING: Potential context gap identified!  Node " + currentNode + " has no edges connected to it!")
+            contextGappedNodes.append(currentNode)
+    return contextGappedNodes
+
+
+# Handles queries asking about the existence of some item: exists(object).  Currently just scans the graph and detects
+# lexical gaps if the object is not found and target gaps if multiple copies of the object are found.
+# No gap resolution yet, just getting detection out the door for now.
+def existenceQuery(queryContents: str, sceneGraph):
+    # Set a flag for if the object queried was found in the node
+    objectFound = False
+    listOfMatchingNodes = []
+    # Get the target of the query
+    queriedObject = queryContents.split(')')[0]
+    # Again, not particularly efficient but hey.
+    for currentNode in sceneGraph:
+        # For each node, strip off the "_#" if present - see assumptions at top of file
+        regexPattern = r'_\d+'
+        nodeObjectName = re.sub(regexPattern, '', currentNode)
+        # If the name of the object matches the target of the query, flag as such and set the flag to true
+        if nodeObjectName == queriedObject:
+            print("Queried object found: " + currentNode)
+            objectFound = True
+            # Also append the node to the list of matching nodes for target gap detection
+            listOfMatchingNodes.append(currentNode)
+    # If nothing matching found, raise a lexical gap
+    if objectFound == False:
+        print("WARNING: Lexical Gap identified - the object queried " + queriedObject +
+              " does not appear in the graph.")
+    # If multiple nodes found that match the queried term, raise a target gap
+    if len(listOfMatchingNodes) > 1:
+        print("WARNING: Potential Target Gap identified.  Multiple nodes match the queried object.  The list of these "
+              "objects is as follows: " + str(listOfMatchingNodes))
+
+def relationQuery(queryContents: str, sceneGraph):
+    pass
+
+
+def main():
+    # Load in the scene graph
+    sceneGraph = networkx.read_graphml(scene_graph_file)
+    # Prior to any querying, check for context gaps on certain items
+    # Returns a list of nodes which have context gaps.  Currently unused but maybe eventually useful?
+    contextGappedNodes = contextGapCheck(sceneGraph)
+    # Get user query.  Eventually need to add attribute handling when attributes are available.
+    userQuery = input("Please enter a query in the format KEYWORD(arguments), with the following options: \n " +
+          existence_keyword + "(object) \n" + relation_keyword + "(relationString,object1,object2) \n")
+    # Break query up into relevant parts
+    queryType = userQuery.split('(', 1)[0]
+    queryContents = userQuery.split('(', 1)[1]
+    # Handle the exists(object) case
+    if queryType == existence_keyword:
+        existenceQuery(queryContents, sceneGraph)
+        pass
+    # Handle the relation(object1,object2) case
+    if queryType == relation_keyword:
+        relationQuery(queryContents, sceneGraph)
+        pass
+    # Handle the attribute case - NOT PRESENT
+    if queryType == attribute_keyword:
+        # ATTRIBUTE CASE
+        pass
+
+    print(sceneGraph)
+
+
+main()
